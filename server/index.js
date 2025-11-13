7170a10edcca4607589da55cb390e56eaf025b52
vema30 <vemareddy1245@gmail.com>
2025-11-14 01:33:27 +0530

    cat

--- server/index.js
+++ server/index.js
@@ -35,14 +35,20 @@ app.use(cookieParser());
 //     allowedHeaders: ["Content-Type", "Authorization"],
 //   })
 // );
+// app.use(cors({
+//   origin: [
+//     "http://localhost:3000",
+//     "https://learning-path-dashboard-for-enhanci.vercel.app"
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://learning-path-dashboard-for-enhanci.vercel.app"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*", // allows all domains
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 
